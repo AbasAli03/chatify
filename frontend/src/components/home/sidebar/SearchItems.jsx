cbef4320126c6e6892a7fd036ac9bb0a9a15c2ed
AbasAli03 <125214662+AbasAli03@users.noreply.github.com>
2024-04-16 21:16:52 +0200

    finished adding the search functionallity
    Need to add a new route for finding chats and creating new ones

--- frontend/src/components/home/sidebar/SearchItems.jsx
+++ frontend/src/components/home/sidebar/SearchItems.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import "./searchItems.css";
+import { useChatContext } from "../../../context/ChatContext";
 
 const SearchItems = ({ data }) => {
+  const { activeChat, setActiveChat } = useChatContext();
+
+  const handleClick = (id, username) => {
+    setActiveChat({
+      chatId: id,
+      participantId: "",
+      participantName: username,
+    });
+  };
+
   return (
     <div>
       {data &&
         data.map((item) => {
-          return <div key={item.id}>data</div>;
+          return (
+            <li
+              key={item.id}
+              className="searchItem"
+              onClick={() => handleClick(item._id, item.username)}
+            >
+              <h1>{item.username}</h1>
+              <p>{item.fullname}</p>
+              <p>Member since: {item.createdAt}</p>
+            </li>
+          );
         })}
     </div>
   );
